a58da77eb92089b30fb169366c0a64c6fdb893f3
Mohammad Al Ramis <MohammadRRamis@outlook.com>
2024-03-20 21:13:01 +0300

    first commit

---
+++ src/div/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.post('/div', (req, res) => {
+  const x = req.body.X;
+  const y = req.body.Y;
+  const result = x / y;
+  res.send({
+    X: x,
+    Y: y,
+    Result: result,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port 8080');
+});
